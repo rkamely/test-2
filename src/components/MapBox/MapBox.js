ff8be6222f629ebdaccd5aa040442a0929c1ac15
bardia1375 <bardiashams1375@gmail.com>
2022-04-13 19:57:19 +0430

    mapBox

--- src/components/MapBox/MapBox.js
+++ src/components/MapBox/MapBox.js
@@ -22,21 +22,21 @@
 
 
 
-// import React, { useState } from "react";
-// import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import React, { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 
-// const MapBox = () => {
-// <MapContainer center={[51.505, -0.09]} zoom={13}>
-//   <TileLayer
-//     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//   />
-//   <Marker position={[51.505, -0.09]}>
-//     <Popup>
-//       A pretty CSS3 popup. <br /> Easily customizable.
-//     </Popup>
-//   </Marker>
-// </MapContainer>
-// };
-// export default MapBox;
+const MapBox = () => {
+<MapContainer center={[51.505, -0.09]} zoom={13}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+};
+export default MapBox;
